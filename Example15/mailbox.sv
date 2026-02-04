78ae1b44268702bf3c4dccdf17767d8e0e261212
sanica19 <sanicams.ec23@bmsce.ac.in>
2026-02-04 11:25:22 +0530

    Added mailbox.sv

---
+++ Example15/mailbox.sv
@@ -0,0 +1,14 @@
+// File        :mailbox.sv
+// Author      :Sanica M S /1BM23EC229
+// Created     :04-02-2026
+// Module      :mailbox
+// Project     :SystemVerilog & Verification(23EC6PE2SV)
+// Faculty     :Prof.Ajaykumar Devarapalli
+// Description :Packet class models randomized transaction data used for communication.
+
+class Packet;
+  rand bit [7:0] val;
+endclass
+
+
+ 
